7e2ec16bab47f52a14d68884a56185370022e655
RSWaffle <ryanthecat17@gmail.com>
2019-03-19 15:04:15 -0400

    Profiling Research Paper init

---
+++ Profiling Research/ProfilingResearch/ProfilingResearch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	File Name:         Program.cs
+//	Description:        Write a short program that fills different list<int>’s of 100 integers with the same random non-negative integer values,
+//                      sorts them using the Sink, Selection, Insertion, Mergesort, Quicksort (original), Quicksort (median-of-three),
+//                      Shell (gap determined using 2.2 as in the lecture), Counting, and Radix (base 10, least significant digit) routines
+
+//	Course:            CSCI 2210 - Data Structures
+//	Author:            Ryan Shupe, shuper@etsu.edu, East Tennessee State University
+//	Created:           Tuesday, Mar 19 2019
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+namespace ProfilingResearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           List<int> intList = new List<int>(100);
+
+        }
+    }
+}
